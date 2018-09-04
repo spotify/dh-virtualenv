a2d1d757c86f53eeee7468d52d8172eb1251904b
jhermann <jh@web.de>
2018-09-04 16:07:56 +0200

    add _version module (closes #178)

---
+++ dh_virtualenv/_version.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Major Project Metadata.
+
+This file should be used to get the version and other metadata whenever
+technically possible, to reduce locations where this data has to be
+maintained redundantly.
+"""
+
+version = '1.1'
+author = u'Jyrki Pulliainen'
+author_email = 'jyrki@spotify.com'
+url = 'https://github.com/spotify/dh-virtualenv'
