77d8b42a3027b3696a02ed8b0aad28f535a957b3
blag <blag@users.noreply.github.com>
2020-12-11 21:06:12 -0800

    Add test for the new shebang string

--- test/test_deployment.py
+++ test/test_deployment.py
@@ -131,6 +131,14 @@ def check_shebangs_fix(interpreter, path):
     with open(temp.name) as f:
         eq_(f.readline(), expected_shebang)
 
+    with open(temp.name, 'w') as f:
+        f.write('#!{0}\n'.format(interpreter))
+
+    deployment.fix_shebangs()
+
+    with open(temp.name) as f:
+        eq_(f.readline(), expected_shebang)
+
     # Additional test to check for paths wrapped in quotes because they contained space
     # Example:
     #           #!"/some/local/path/dest/path/bin/python"
